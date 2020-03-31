a62aade35c2d6ff3052160d77d2e5757bb1b3dac
N-Ziermann <n-ziermann@protonmail.com>
2020-03-31 22:20:55 +0200

    Basic structure

--- minigame_site/src/App.js
+++ minigame_site/src/App.js
@@ -2,25 +2,63 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload page!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super (props);
+  }
+  render(){
+    return (
+      <div className="App">
+        <Header />
+        <Carrousel />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+class Carrousel extends React.Component{
+  constructor(props){
+    super (props);
+    this.state = {
+      position:3,
+    }
+  }
+  render(){
+    return (
+      <div>
+      {
+        this.state.position == 1
+        ? <div>First Element</div>
+        : this.state.position == 2
+        ? <div>Second Element</div>
+        : <div>Third Element</div>
+      }
+      </div>
+    )
+  }
+}
+
+class Header extends React.Component{
+  constructor(props){
+    super (props);
+  }
+  render(){
+    return(
+      <h1>Minigame-Collection-App</h1>
+    );
+  }
+}
+
+class Footer extends React.Component{
+  constructor(props){
+    super (props);
+  }
+  render(){
+    return(
+      <p>Made in Godot</p>
+    );
+  }
 }
 
 export default App;
