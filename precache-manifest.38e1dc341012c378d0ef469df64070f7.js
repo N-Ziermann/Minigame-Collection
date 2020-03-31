76888dbc6da042c2151ea8fb0e7c9a0cdb21e7d0
Niklas Ziermann <n-ziermann@protonmail.com>
2020-03-31 14:46:48 +0200

    Updates

---
+++ precache-manifest.38e1dc341012c378d0ef469df64070f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c9982b20b13b25166c983bdf6667f9",
+    "url": "/Minigame-Collection/index.html"
+  },
+  {
+    "revision": "71380138ca29a647b67e",
+    "url": "/Minigame-Collection/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "816e45b16a32c315e682",
+    "url": "/Minigame-Collection/static/js/2.52a955b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Minigame-Collection/static/js/2.52a955b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71380138ca29a647b67e",
+    "url": "/Minigame-Collection/static/js/main.25c56c8c.chunk.js"
+  },
+  {
+    "revision": "aa330e8adb366e2b262a",
+    "url": "/Minigame-Collection/static/js/runtime-main.79204143.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Minigame-Collection/static/media/logo.5d5d9eef.svg"
+  }
+]);
