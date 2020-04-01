b240e76afc6253d1901a491a349ca7e831b2bf2c
N-Ziermann <n-ziermann@protonmail.com>
2020-04-01 15:25:14 +0200

    Implement Carrousel turning

--- minigame_site/src/App.js
+++ minigame_site/src/App.js
@@ -12,7 +12,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <Carrousel />
+        <Carrousel items={["Dog","Cat","Bird"]}/>
         <Footer />
       </div>
     );
--- minigame_site/src/Components/Carrousel.js
+++ minigame_site/src/Components/Carrousel.js
@@ -4,24 +4,48 @@ class Carrousel extends React.Component{
   constructor(props){
     super (props);
     this.state = {
-      position:2,
+      position:0,
+    }
+    this.turn_carrousel = this.turn_carrousel.bind(this);
+  }
+
+  turn_carrousel(turn_by){
+    // if conditions prevent over-/underflow
+    if (turn_by > 0){
+      if (this.state.position < this.props.items.length-1){
+        this.setState(state=>({
+          position : state.position + turn_by
+        }));
+      }
+      else{
+        this.setState({
+          position:0
+        });
+      }
+    }
+
+    else if (turn_by < 0){
+      if (this.state.position > 0){
+        this.setState(state=>({
+          position : state.position + turn_by
+        }));
+      }
+      else{
+        this.setState({
+          position:this.props.items.length-1
+        });
+      }
     }
   }
+
   render(){
     return (
       <div>
-      {
-        this.state.position == 1
-        ? <div>First Element</div>
-        : this.state.position == 2
-        ? <div>Second Element</div>
-        : <div>Third Element</div>
-      }
-      <br/>
-      <button onClick={function(){console.log("a")}}>{"<--"}</button>
-      <button onClick={function(){console.log("b")}}>{"-->"}</button>
+        <h3>{this.props.items[this.state.position]}</h3>
+        <button onClick={() => this.turn_carrousel(-1)}>{"<--"}</button>
+        <button onClick={() => this.turn_carrousel(1)}>{"-->"}</button>
       </div>
-    )
+    );
   }
 }
 
