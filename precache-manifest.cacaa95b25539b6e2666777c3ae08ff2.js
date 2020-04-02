33f619b3ff97fbf6a7343e1f592813197e246bda
N-Ziermann <n-ziermann@protonmail.com>
2020-04-02 19:09:58 +0000

    Automated Release

---
+++ precache-manifest.cacaa95b25539b6e2666777c3ae08ff2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0431f20fd939ccfeabbb57e779d77d77",
+    "url": "/Minigame-Collection/index.html"
+  },
+  {
+    "revision": "5794ad97be40e9b761eb",
+    "url": "/Minigame-Collection/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4114ce8c4947abd2fb17",
+    "url": "/Minigame-Collection/static/css/main.3fe6da62.chunk.css"
+  },
+  {
+    "revision": "5794ad97be40e9b761eb",
+    "url": "/Minigame-Collection/static/js/2.e6890b61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Minigame-Collection/static/js/2.e6890b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4114ce8c4947abd2fb17",
+    "url": "/Minigame-Collection/static/js/main.2367dc1a.chunk.js"
+  },
+  {
+    "revision": "aa330e8adb366e2b262a",
+    "url": "/Minigame-Collection/static/js/runtime-main.79204143.js"
+  },
+  {
+    "revision": "fac063b74cefdd8be0e9dc3bde7f43e2",
+    "url": "/Minigame-Collection/static/media/clean_screen.fac063b7.png"
+  },
+  {
+    "revision": "5f0bd832720a9461a36f9d9adc8b5790",
+    "url": "/Minigame-Collection/static/media/find_cactus.5f0bd832.png"
+  },
+  {
+    "revision": "6ac593b55af435f7bf329b59dc80a12e",
+    "url": "/Minigame-Collection/static/media/magnifying_glass.6ac593b5.png"
+  },
+  {
+    "revision": "dc212caa986e15712b2e894db348f432",
+    "url": "/Minigame-Collection/static/media/mole_tab.dc212caa.png"
+  },
+  {
+    "revision": "6ce119ec0e84b7efad6cbbf719caa20e",
+    "url": "/Minigame-Collection/static/media/photo_ufo.6ce119ec.png"
+  }
+]);
