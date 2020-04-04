90180d5772a3166d037ca9da235a8300aad7027a
N-Ziermann <n-ziermann@protonmail.com>
2020-04-04 20:11:16 +0200

    Made Download Button Responsive

--- minigame_site/src/Pages/Play.js
+++ minigame_site/src/Pages/Play.js
@@ -48,14 +48,19 @@ class Play extends React.Component {
 const DownloadButton = () => {
   return (
     <div className="row">
-      <div className="col-lg-4"/>
-      <div className="col-lg-4 row">
-        <div className="col-lg-3"/>
-        <a href="https://github.com/N-Ziermann/Minigame-Collection/releases/download/v_1.0.0/Minigame-Collection.apk" className="row col-lg-8" style={{backgroundColor:"green", padding:"15px", borderRadius:10, textDecoration:"none", color:"black", border:"solid"}}>
-          <img style={{width:"20%"}} src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fd%2Fdb%2FAndroid_robot_2014.svg%2F872px-Android_robot_2014.svg.png&f=1&nofb=1"}/>
-          <h5 style={{marginLeft:20}}>Download for <br/>Android</h5>
+      <div className="col-lg-4 col-md-1 col-2"/>
+      <div className="col-lg-4 col-md-10 col-10 row">
+        <div className="col-lg-2 col-md-3 col-0"/>
+        <a href="https://github.com/N-Ziermann/Minigame-Collection/releases/download/v_1.0.0/Minigame-Collection.apk" className="col-lg-10 col-md-7 col-12 row" style={{backgroundColor:"#25383C", padding:"15px", borderRadius:10, textDecoration:"none", color:"white", border:"solid black"}}>
+          <div className="col-lg-4 col-md-4 col-5">
+            <img style={{width:"100%"}} src={"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fd%2Fdb%2FAndroid_robot_2014.svg%2F872px-Android_robot_2014.svg.png&f=1&nofb=1"}/>
+          </div>
+          <div className="col-lg-8 col-md-8 col-7" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+            <h5  style={{marginBottom:0}}>Download<br/>for<br/>Android</h5>
+          </div>
         </a>
       </div>
+      <div className="col-lg-4 col-1"/>
     </div>
   );
 }
